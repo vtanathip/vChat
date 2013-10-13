5e2a52ecbd8331cc7585b4da58007854061f3cb6
Vtanathip <tanathip.se@gmail.com>
2013-10-13 16:16:25 +0700

    testing heroku mongohq

--- chat-server.js
+++ chat-server.js
@@ -12,6 +12,20 @@ var  http    = require('http')
 
 // array for keep username list
 var vClient = new Object();
+var mongo = require('mongodb');
+
+//db server uri
+var mongoUri = process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/vchat';
+
+//connect to database
+mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('mydocs', function(er, collection) {
+        collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+        });
+    });
+});
 
 // listening to port...
 server.listen(process.env.PORT || port, function(){
